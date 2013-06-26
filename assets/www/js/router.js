8eee7603e1909db9ae4530a61fe7035dd89fc449
Marius Fink <github@mariusfink.org>
2013-06-26 17:23:40 +0200

    readme2

---
+++ assets/www/js/router.js
@@ -0,0 +1,72 @@
+define([
+  'jquery',
+  'jquerymobile',
+  'underscore',
+  'backbone',
+  'views/add-navbar',
+  'views/data'
+  //'views/add',
+  //'views/legend'
+  ],
+  function($, jqm, _, Backbone, AddNavbar, DataView){ //, addView, legendView
+    console.log("router loaded");
+    var AppRouter = Backbone.Router.extend({
+      routes: {
+        'add/browser': 'view', 
+        //main routes
+        '':            'view',
+        'view':        'view',
+        'add':         'add',
+        'legend':      'legend',
+        '*actions':    'defaultAction'
+      }
+    });
+
+    var initialize = function(){
+      var router = new AppRouter;
+      var lastView = "";
+      router.on('all', function(route) {
+        lastView = Backbone.history.fragment;
+      }, this);
+
+      router.on('route:view', function (){
+        console.log("route:view from: " + lastView);
+        DataView.render();
+        jqm.changePage( "#view", {
+          transition: "slide",
+          reverse: false,
+          changeHash: false
+        });
+      });
+      //Main Routing
+      router.on('route:add', function() {
+        console.log("route:add from: " + lastView);
+        jqm.changePage( "#add", {
+          transition: "slide",
+          reverse: false,
+          changeHash: false
+        });
+      });
+      router.on('route:legend', function() {
+        console.log("route:legend from: " + lastView);
+        jqm.changePage( "#legend", {
+          transition: "slide",
+          reverse: true,
+          changeHash: false
+        });
+      });
+      router.on('defaultAction', function(actions){
+        // We have no matching route, lets just log what the URL was 
+        console.log('No matching route for: ', actions);
+      });
+
+      new AddNavbar();
+
+      Backbone.history.start();
+    };
+
+    return {
+      initialize: initialize
+    };
+  }
+);
