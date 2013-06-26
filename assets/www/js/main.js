9d6cab59251f4e87f74c1f25400205ec4f188417
Marius Fink <github@mariusfink.org>
2013-06-26 12:55:18 +0200

    adding the first view (w debug alerts)

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,33 +1,25 @@
 /*
-  <!-- JQUERY -->
-  <script src="js/libs/jquery-1.7.2.min.js"></script>
-  <!-- Jquery Mobile -->
-  <script src="js/libs/jquery.mobile/jquery.mobile-1.3.1.js"></script>
-  <!-- Phonegap 
-  <script src="js/libs/phonegap/cordova-1.9.0.js"></script>
-  <!- - JQuery Geo 
-  <script src="js/libs/jquery.geo-1.0b1.min.js"></script>
-  -->
-  <!-- Backbone -->
-  <script src="js/libs/underscore.js"></script>
-  <script src="js/libs/backbone-1.0.0.js"></script>
-
-  <!-- Phonegap API -->
-  <!--
-  <script src="js/libs/phonegap/apis/accelerometer.js"></script>
-  <script src="js/libs/phonegap/apis/camera.js"></script>
-  <script src="js/libs/phonegap/apis/capture.js"></script>
-  <script src="js/libs/phonegap/apis/compass.js"></script>
-  <script src="js/libs/phonegap/apis/connection.js"></script>
-  <script src="js/libs/phonegap/apis/contacts.js"></script>
-  <script src="js/libs/phonegap/apis/device.js"></script>
-  <script src="js/libs/phonegap/apis/events.js"></script>
-  <script src="js/libs/phonegap/apis/file.js"></script>
-  <script src="js/libs/phonegap/apis/geolocation.js"></script>
-  <script src="js/libs/phonegap/apis/media.js"></script>
-  <script src="js/libs/phonegap/apis/notification.js"></script>
-  <script src="js/libs/phonegap/apis/storage.js"></script>
-  -->
+--vendor
+js/libs/jquery-1.7.2.min.js
+js/libs/jquery.mobile/jquery.mobile-1.3.1
+js/libs/phonegap/cordova-1.9.0
+js/libs/jquery.geo-1.0b1.min
+js/libs/underscore
+js/libs/backbone-1.0.0
+--phonegap api
+js/libs/phonegap/apis/accelerometer
+js/libs/phonegap/apis/camera
+js/libs/phonegap/apis/capture
+js/libs/phonegap/apis/compass
+js/libs/phonegap/apis/connection
+js/libs/phonegap/apis/contacts
+js/libs/phonegap/apis/device
+js/libs/phonegap/apis/events
+js/libs/phonegap/apis/file
+js/libs/phonegap/apis/geolocation
+js/libs/phonegap/apis/media
+js/libs/phonegap/apis/notification
+js/libs/phonegap/apis/storage
 */
 
 require.config({ 
@@ -55,7 +47,7 @@ require([
     'backbone'
   ], 
   function($, jqm, _, Backbone){
-    // Prevents all anchor click handling, prevents jQuery Mobile from handling hash changes
+    // Prevents all anchor click and hash change handling
     $.mobile.linkBindingEnabled = false;
     $.mobile.hashListeningEnabled = false;
 
