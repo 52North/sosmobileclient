b19af12e73284a03f786d3b654ca87fabc9d83fc
Marius Fink <github@mariusfink.org>
2013-07-21 09:40:46 +0200

    adds logos and images

--- assets/www/js/collections/services.js
+++ assets/www/js/collections/services.js
@@ -1,7 +1,7 @@
 var Services = Backbone.Collection.extend({
   model: Service,
-  //url: 'http://sensorweb.demo.52north.org/sensorwebclient-webapp-stable/api/v0/services.json',
-  url: 'js/_json/services.json',
+  url: 'http://sensorweb.demo.52north.org/sensorwebclient-webapp-stable/api/v0/services.json',
+  //url: 'js/_json/services.json',
 
   initialize: function() {
   	this.fetch();
