e6da380035896e46fe5310d92b8b57d5139d6743
Marius Fink <github@mariusfink.org>
2013-08-07 20:33:04 +0200

    legend shows current timeseries

--- assets/www/js/helpers/util.js
+++ assets/www/js/helpers/util.js
@@ -1,6 +1,14 @@
 function generateStationsUrl(provider) {
   if (!provider) {
-  	provider = "PEGELONLINE";
+    provider = "PEGELONLINE";
   }
   return "http://sensorweb.demo.52north.org/sensorwebclient-webapp-stable/api/v0/services/" + provider.toString() + "/stations.json";
+}
+
+function collectionModelsToJSONArray(collection) {
+  json_array = [];
+  _.each(collection.models, function (elem) {
+  	json_array.push(elem.toJSON());
+  });
+  return json_array;
 }
--- assets/www/js/views/legend.js
+++ assets/www/js/views/legend.js
@@ -2,13 +2,12 @@ var LegendView = Backbone.View.extend({
   el: '#legend-content',
 
   initialize: function(){
-    this.listenTo(this.collection, 'change', this.render);
+    this.listenTo(this.collection, 'all', this.render);
   },
 
   render: function() {
     var template = Handlebars.helpers.getTemplate('legend');
-    var listHtml = template({'currentTimeseries': this.collection.models});
-
+    var listHtml = template({'currentTimeseries': collectionModelsToJSONArray(this.collection)});
     this.$el.empty().append(listHtml);
   }
 
