280ff40fedf3457686a4e713d66cac8983281550
Marius Fink <github@mariusfink.org>
2013-08-07 08:44:12 +0200

    adds toggle swicth with geolocation watch

--- assets/www/js/views/map.js
+++ assets/www/js/views/map.js
@@ -1,21 +1,24 @@
 var MapView = Backbone.View.extend({ 
 
+  events: {
+    'click .posBtn': 'toggleTracking'
+  },
+
   initialize: function(){
     this.collection = this.options.stations;
     me = this;
     me.listenTo(me.collection, 'reset', me.render);
     me.collection.fetch({reset: true});
     
-    //navigator.geolocation.watchPosition(this.updateLocation, this.onError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+    tracking = false;
   },
-
 
   render: function() {
     map = $("<div>");
     map.attr("id", "map");
     this.$el.append(map);
 
-    map.geomap({
+    this.map = map.geomap({
       center : [ 7.652469,51.934145 ],
       zoom : 10,
       mode: "find",
@@ -38,17 +41,42 @@ var MapView = Backbone.View.extend({
 
     stations = this.collection;
     _.forEach(stations, function(index, model, list){
-      map.geomap("append", stations.at(model).get('geometry'), { color: "#bb0000", width: 10, height: 10, borderRadius: 10 }, false);
+      this.map.geomap("append", stations.at(model).get('geometry'), { color: "#bb0000", width: 10, height: 10, borderRadius: 10 }, false);
     });
 
+    //my position switch
+    posBtnWrapper = $("<div>").addClass('map-buttons');
+    this.posBtn = $("<a>").addClass('btn posBtn');
+    this.updatePosBtn();
+    this.posBtn.html("<i class='icon-map-marker'></i>");
+    posBtnWrapper.append(this.posBtn);
+
+    this.$el.append(posBtnWrapper);
+  },
+  updatePosBtn: function() {
+    if (tracking) {
+      this.posBtn.removeClass('btn-default');
+      this.posBtn.addClass('btn-primary');
+    } else {
+      this.posBtn.removeClass('btn-primary');
+      this.posBtn.addClass('btn-default');
+    }
+  },
+  toggleTracking: function(event) {
+    tracking = !tracking;
+    if (tracking) {
+      this.watchId = navigator.geolocation.watchPosition(this.updateLocation, this.onError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+    } else {
+      navigator.geolocation.clearWatch(this.watchId);
+    }
+    this.updatePosBtn();
   },
   updateLocation: function(position) {
     coord = [position.coords.longitude, position.coords.latitude];
-    if (this.$el) {
-      var accuracyBuffer = position.coords.accuracy / this.$el.geomap("option", "pixelSize");
-      this.$el.geomap("append", { type: "Point", coordinates: coord }, { color: "#cc0", width: accuracyBuffer, height: accuracyBuffer, borderRadius: accuracyBuffer }, false);
-      this.$el.geomap("option", "center", coord);
-
+    if (this.map) {
+      var accuracyBuffer = position.coords.accuracy / this.map.geomap("option", "pixelSize");
+      this.map.geomap("append", { type: "Point", coordinates: coord }, { color: "#2176B7", width: accuracyBuffer, height: accuracyBuffer, borderRadius: accuracyBuffer }, false);
+      this.map.geomap("option", "center", coord);
     }
   },
   onError: function(error) {
@@ -74,9 +102,6 @@ var MapView = Backbone.View.extend({
         outputHtml += ("Found a " + this.type + " at " + this.coordinates );
       });
       alert(outputHtml);
-    }
-
-
-   
+    }   
   }
 });
