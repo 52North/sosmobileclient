50097f2f7e9f0045ed28f094130e72c113b1dac9
marfnk <github@mariusfink.org>
2013-08-12 11:56:02 +0200

    removes highcharts, adds rickshaw

--- assets/www/js/views/map.js
+++ assets/www/js/views/map.js
@@ -8,8 +8,8 @@ var MapView = Backbone.View.extend({
     me.listenTo(this.collection, 'reset', me.drawStations);
 
     Backbone.Mediator.subscribe('station:locate', function(timeseries) {
-      me.map.geomap("option", "center", timeseries.get('location').coordinates);
-      me.map.geomap("option", "zoom", 16)
+      me.map.geomap("option", "center", timeseries.get('location'));
+      me.map.geomap("option", "zoom", 12)
     }, this);
   },
 
