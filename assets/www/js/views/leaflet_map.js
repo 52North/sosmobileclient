642e5a0da12ca914d59986bedb98203098b3911f
marfnk <github@mariusfink.org>
2013-08-20 19:22:08 +0200

    finishes leaflet map

--- assets/www/js/views/leaflet_map.js
+++ assets/www/js/views/leaflet_map.js
@@ -18,12 +18,10 @@ var MapView = Backbone.View.extend({
   render: function() {
     map = $("<div>");
     map.attr("id", "map");
-    map.width(this.$el.parent.width);
-    map.height(500);
     this.$el.empty().append(map);
 
     var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
-      cloudmadeAttribution = '',
+      cloudmadeAttribution = '&copy; OpenStreetMap &amp; Contributors',
       cloudmade = L.tileLayer(cloudmadeUrl, {maxZoom: 17, attribution: cloudmadeAttribution}),
       latlng = L.latLng(50, 10);
     L.Icon.Default.imagePath = '/img';
@@ -49,7 +47,7 @@ var MapView = Backbone.View.extend({
       var markers = this.markers;
       _.each(this.collection.models, function (elem, index) {
         var geom = elem.get('geometry').coordinates;
-        var marker = L.marker(new L.LatLng(geom[1], geom[0]), { title: "title" });
+        var marker = L.marker(new L.LatLng(geom[1], geom[0]), { /* title: 'station name' */ });
         marker.on('click', me.findAndAdd);
         markers.addLayer(marker);
       });
@@ -69,17 +67,25 @@ var MapView = Backbone.View.extend({
     me.posBtn.find('#posBtnIcon').removeClass("icon-spinner icon-spin");
     me.posBtn.find('#posBtnIcon').addClass("icon-screenshot");
 
-    coord = [position.coords.longitude, position.coords.latitude];
-    if (this.map) {
-      //me.markerLayer.geomap("remove", me.point);
-      me.point = { type: "Point", coordinates: coord };
-
-      //me.markerLayer.geomap( "append", me.point, "" );
-
-      //this.map.geomap("option", "center", coord);
-      //this.map.geomap("option", "zoom", 12)
+    if (me.map.hasLayer(me.yourPos)) {
+      me.map.removeLayer(me.yourPos);
     }
 
+    var myIcon = L.icon({
+      iconUrl: '/img/marker.png',
+      shadowUrl: '/img/marker-shadow.png',
+
+      iconSize:     [40, 46], // size of the icon
+      shadowSize:   [41, 41], // size of the shadow
+      iconAnchor:   [20, 46], // point of the icon which will correspond to marker's location
+      shadowAnchor: [10, 41],  // the same for the shadow
+      popupAnchor:  [0, -46] // point from which the popup should open relative to the iconAnchor
+    });
+
+    me.yourPos = L.marker(new L.LatLng(position.coords.latitude, position.coords.longitude), { title: "You are here.", icon: myIcon });
+    me.map.addLayer(me.yourPos);
+
+    me.map.fitBounds([[position.coords.latitude, position.coords.longitude],[position.coords.latitude, position.coords.longitude]]);
   },
   onError: function(error) {
     me.posBtn.find('#posBtnIcon').removeClass("icon-spinner icon-spin");
