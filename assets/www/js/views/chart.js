b2cd9c4a6cce888fd6ee40990a1dd4bd8122534c
marfnk <github@mariusfink.org>
2013-08-31 22:21:10 +0200

    adds beautiful flot example

--- assets/www/js/views/chart.js
+++ assets/www/js/views/chart.js
@@ -10,6 +10,10 @@ var ChartView = Backbone.View.extend({
 
   render: function() {   
     this.$el.empty();
+    
+    this.initialRenderChart();
+
+
     if (this.collection.anythingVisible()) {
       //Todo loading screen!
     } else {
@@ -27,12 +31,77 @@ var ChartView = Backbone.View.extend({
       //add 
     } else {
       //render with this
-      initialRenderChart(data);
     }
   },
 
-  initialRenderChart: function() {
+     sumf: function(f, t, m) {
+      var res = 0;
+      for (var i = 1; i < m; ++i) {
+        res += f(i * i * t) / (i * i);
+      }
+      return res;
+    },
 
+  initialRenderChart: function() {
+    // generate data set from a parametric function with a fractal look
+
+
+    var d1 = [];
+    for (var t = 0; t <= 2 * Math.PI; t += 0.01) {
+      d1.push([this.sumf(Math.cos, t, 10), this.sumf(Math.sin, t, 10)]);
+    }
+
+    var data = [ d1 ];
+
+    var plot = $.plot(this.$el, data, {
+      series: {
+        lines: {
+          show: true
+        },
+        shadowSize: 0
+      },
+      xaxis: {
+        zoomRange: [0.1, 10],
+        panRange: [-10, 10]
+      },
+      yaxis: {
+        zoomRange: [0.1, 10],
+        panRange: [-10, 10]
+      },
+      zoom: {
+        interactive: true
+      },
+      pan: {
+        interactive: true
+      }
+    });
+
+    // show pan/zoom messages to illustrate events 
+
+    this.$el.bind("plotpan", function (event, plot) {
+      var axes = plot.getAxes();
+      $(".message").html("Panning to x: "  + axes.xaxis.min.toFixed(2)
+      + " &ndash; " + axes.xaxis.max.toFixed(2)
+      + " and y: " + axes.yaxis.min.toFixed(2)
+      + " &ndash; " + axes.yaxis.max.toFixed(2));
+    });
+
+    this.$el.bind("plotzoom", function (event, plot) {
+      var axes = plot.getAxes();
+      $(".message").html("Zooming to x: "  + axes.xaxis.min.toFixed(2)
+      + " &ndash; " + axes.xaxis.max.toFixed(2)
+      + " and y: " + axes.yaxis.min.toFixed(2)
+      + " &ndash; " + axes.yaxis.max.toFixed(2));
+    });
+
+    // add zoom out button 
+
+    $("<div class='button' style='right:20px;top:20px'>zoom out</div>")
+      .appendTo(this.$el)
+      .click(function (event) {
+        event.preventDefault();
+        plot.zoomOut();
+      });
   }
      
 });
