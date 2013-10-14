8c62f676096ebc46003a4601cc72ef10e9bb6852
marfnk <github@mariusfink.org>
2013-10-14 17:39:26 +0200

    fixes + splashscreen

--- src/org/n52/sosmobileclient/MobileSWC.java
+++ src/org/n52/sosmobileclient/MobileSWC.java
@@ -30,7 +30,11 @@ public class MobileSWC extends CordovaActivity
         super.onCreate(savedInstanceState);
         super.init();
         // Set by <content src="index.html" /> in config.xml
+        
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl(Config.getStartUrl());
+        
+        //super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
     }
 }
